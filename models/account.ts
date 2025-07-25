a41f3c19081c38876916bd921f9b5cc3dcde212d
ZenithSUS <jeranmerino147@gmail.com>
2025-07-25 22:34:32 +0800

    feat: add account and user models with Mongoose schemas

---
+++ models/account.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { Account } from "../types/account";
+
+const accountSchema = new mongoose.Schema<Account>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["active", "inactive", "deleted"],
+    default: "active",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  lastLogin: {
+    type: Date,
+    default: null,
+  },
+});
+
+export default mongoose.model("Account", accountSchema);
---
+++ models/user.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { User } from "../types/user";
+
+const userSchema = new mongoose.Schema<User>({
+  accountId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  goals: {
+    loseWeight: {
+      type: Boolean,
+      required: false,
+    },
+    gainWeight: {
+      type: Boolean,
+      required: false,
+    },
+    maintainWeight: {
+      type: Boolean,
+      required: false,
+    },
+  },
+});
+
+export default mongoose.model("User", userSchema);
