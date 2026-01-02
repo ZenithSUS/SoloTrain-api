0bf4c03a133888100d7f0492b094c8c54449f2bd
ZenithSUS <jeranmerino147@gmail.com>
2026-01-03 03:04:21 +0800

    remove: gmail service and change the port to 587

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -3,9 +3,8 @@ import config from "../config.js";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
-  service: "gmail",
-  secure: true,
-  port: 465,
+  secure: false,
+  port: 587,
   auth: {
     user: "apikey",
     pass: config.sendGridApiKey,
