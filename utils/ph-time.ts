c5b51ce428158700be937efb0c718ed463c75b2f
ZenithSUS <jeranmerino147@gmail.com>
2026-01-08 22:28:50 +0800

    fix: ph time format bug where it add more 8 hours.

--- utils/ph-time.ts
+++ utils/ph-time.ts
@@ -1,13 +1,7 @@
 function phTime() {
-  const now = new Date();
-
-  // Get UTC timestamp
-  const utcTime = now.getTime();
-
-  // Add 8 hours (Philippine time offset)
-  const phTime = new Date(utcTime + 8 * 60 * 60 * 1000);
-
-  return phTime;
+  return new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Manila" })
+  );
 }
 
 export default phTime;
